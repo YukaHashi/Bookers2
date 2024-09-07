019d84e50a334cb8fcd0a8a729cae56b72414c0d
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-09-07 14:46:13 +0000

    [Add]1-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,27 @@
 class BooksController < ApplicationController
   def new
+    # viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @books = Book.new
+  end
+  
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
   end
 
   def show
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+  
+  def book_params
+    oarams.require(:book).permit(:book, :image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   
-  resources :books, only:[:new, :index, :show]
+  resources :books, only:[:new, :create, :index, :show]
+  
+  # post '/books/new' => "books#new", as: "new_book"
   
   get '/homes/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
