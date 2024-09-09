afcf58fec211f3eaa30d8cc9576fb1a09a6d6e75
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-09-09 13:41:54 +0000

    [Add]1-11

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,11 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
   
   # 投稿データのストロングパラメータ
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
   
   resources :books, only:[:new, :create, :index, :show]
   
-  # post '/books/new' => "books#new", as: "new_book"
-  
   get '/homes/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
