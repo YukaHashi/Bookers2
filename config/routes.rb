a26b7446cf20ef173e0d3f7a2a4b88243f4ec539
YukaHashi <yuka.hashimotorenge@gmail.com>
2024-09-27 13:22:53 +0000

    [Add]1-25

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :books, only:[:new, :create, :index, :show, :destroy]
   resources :users, only:[:show, :edit, :update]
   
-  get '/homes' => "homes#top"
+  get '/homes/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
